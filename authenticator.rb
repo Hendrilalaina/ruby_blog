c72d4ffdc57080b39ac38f4286177b228288d0ae
hendrilalaina <hendrilalaina@gmail.com>
2024-06-20 17:02:31 +0300

    Authenticator

---
+++ authenticator.rb
@@ -0,0 +1,35 @@
+puts "Welcome to the auttenticator"
+puts "-"*28
+
+def connect
+  puts
+  print "Username: "
+  uname = gets.chomp
+  print "Password: "
+  passwd = gets.chomp
+  puts
+  authenticate(uname, passwd)
+end
+
+def authenticate(uname, passwd)
+  users = [
+    {username: "user1", password: "password1"},
+    {username: "user2", password: "password2"},
+    {username: "user3", password: "password3"},
+    {username: "user4", password: "password4"}
+  ]
+  users.each do |user|
+    if user[:username] == uname
+        if user[:password] == passwd
+          puts "You are connected as #{uname}!"
+          return
+        else
+          puts "Credentials are not matched!"
+          connect
+        end
+    end
+  end
+  puts "Unvalid username!"
+  connect
+end
+connect
